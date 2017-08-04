5840ffd20b9fba5de20ae199711f58ba51235f36
Edwin Gamboa <edwjov92@gmail.com>
2017-08-04 09:45:04 -0500

    CDT-62: Add bin script

---
+++ bin/cubx-generate-webpackage-readme-file.js
@@ -0,0 +1,18 @@
+'use strict';
+var WebpackageReadmeGenerator = require('../lib/cubx-generate-webpackage-readme-file');
+var commandLineArgs = require('command-line-args');
+
+var optionDefinitions = [
+  { name: 'webpackagePath', type: String, defaultOption: true, alias: 'p' },
+  { name: 'storeName', type: String, alias: 's' },
+  { name: 'includeSample', type: Boolean },
+  { name: 'includeInit', type: Boolean },
+  { name: 'sampleArtifactId', type: String },
+  { name: 'sampleSlotName', type: String },
+  { name: 'sampleSlotValue', type: String }
+];
+
+var options = commandLineArgs(optionDefinitions);
+var generator = new WebpackageReadmeGenerator(options.webpackagePath);
+generator.setTemplateValues(options);
+generator.generateReadmeFile();
